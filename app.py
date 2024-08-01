fc0497cba05e2435e0f5b126a63d9f32d1cbb7d1
AYUSH MISHRA <ayushm.ug21.cs@nitp.ac.in>
2024-08-01 11:06:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -187,10 +187,18 @@ with chat_container:
 
 # Input for user prompt (Positioned at the bottom)
 with st.container():
-    user_input = st.text_input("Enter your message:", "", key="input")
+    user_input = ""
 
     if "user_input" not in st.session_state:
         st.session_state.my_text = ""
+
+    def submit():
+        st.session_state.my_text = st.session_state.widget
+        st.session_state.widget = ""
+    st.text_input("Enter text here", key="widget", on_change=submit)
+    #my_text = st.session_state.my_text
+    #st.write(my_text)
+
         
     if user_input:
         # Add user message to chat history
